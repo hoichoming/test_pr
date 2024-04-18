d86a6798d5256ceabb964042cb603a00dfd2e197
Hoicho Ming <hoicho.ming@nyshex.com>
2024-04-18 15:27:50 -0400

    update

--- src/utils/dbconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DATABASE_CONFIG = {
-    'user': os.environ['DB_USER'],
-    'password': os.environ['DB_PASSWORD'],
-    'host': os.environ['DB_HOST'],
-    'dbname': 'postgres',
-    'port': '5432',
-}
